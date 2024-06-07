28a5d10280f0fbd8d8a70d4cc6f160c85d18612f
Gadg8eer <kidpunk@gadg8eer.top>
2024-06-06 19:31:25 -0600

    Updated basic files
    Updated the main directory. This may or may not be only a temporary change, the release might not have some of these files.

--- mod_sr_compat_set_BUILD.bat
+++ mod_sr_compat_set_BUILD.bat
@@ -2,8 +2,9 @@
 @RD /S /Q ".\.nmlcache"
 @RD /S /Q ".\backups"
 @DEL /S /Q ".\mod_sr_compat_set.grf"
-@DEL /S /Q ".\mod_sr_compat_set.nml"
 :start
 python nml_patcher.py -f "mod_sr_compat_set.pnml" -o "mod_sr_compat_set.nml" -b 1 -v 1
 nmlc mod_sr_compat_set.nml -o mod_sr_compat_set.grf
+@DEL /S /Q ".\mod_sr_compat_set.nml"
 PAUSE
+goto start
