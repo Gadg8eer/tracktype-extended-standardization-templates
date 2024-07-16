a5f02d7d780af90825fb896a5beccfac2c383669
Gadg8eer <64449092+Gadg8eer@users.noreply.github.com>
2024-07-16 01:40:13 -0600

    Merged old files
    Removed unneeded documents.

--- pybuild (2).bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@RD /S /Q ".\.nmlcache"
-@RD /S /Q ".\backups"
-@DEL /S /Q ".\mod_sr_compat_set.grf"
-@DEL /S /Q ".\mod_sr_compat_set.nml"
-:start
-python nml_patcher.py -f "mod_sr_compat_set.pnml" -o "mod_sr_compat_set.nml" -b 1 -v 1
-nmlc mod_sr_compat_set.nml -o mod_sr_compat_set.grf
-PAUSE
