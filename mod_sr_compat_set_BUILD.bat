879b5b5da783ac8efee71a7bb61f5452f2bc5e2b
Gadg8eer <64449092+Gadg8eer@users.noreply.github.com>
2024-09-17 15:36:39 -0600

    Update mod_sr_compat_set_BUILD.bat

--- mod_sr_compat_set_BUILD.bat
+++ mod_sr_compat_set_BUILD.bat
@@ -3,8 +3,8 @@
 @RD /S /Q ".\backups"
 @DEL /S /Q ".\mod_sr_compat_set.grf"
 :start
+@DEL /S /Q ".\mod_sr_compat_set.nml"
 python nml_patcher.py -f "mod_sr_compat_set.pnml" -o "mod_sr_compat_set.nml" -b 1 -v 1
 nmlc mod_sr_compat_set.nml -o mod_sr_compat_set.grf
-@DEL /S /Q ".\mod_sr_compat_set.nml"
 PAUSE
 goto start
