2c05dcbda13b1d3c310c4ab40dcff7385bf8f3a5
Gadg8eer <64449092+Gadg8eer@users.noreply.github.com>
2025-02-12 13:45:08 -0700

    Create mod_test_BUILD.bat

---
+++ mod_test_BUILD.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+@RD /S /Q ".\.nmlcache"
+@RD /S /Q ".\backups"
+@DEL /S /Q ".\mod_test.grf"
+:start
+@DEL /S /Q ".\mod_test.nml"
+python nml_patcher.py -f "mod_test.pnml" -o "mod_test.nml" -b 1 -v 1
+nmlc mod_test.nml -o mod_test.grf
+PAUSE
+goto start
